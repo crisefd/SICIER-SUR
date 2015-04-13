4685a7c9e9f5673fc898da541d153d7df90e5a65
danieldha <daniel.henao@correounivalle.edu.co>
2015-04-13 15:09:44 -0500

    modificacion de la conexion en la fachada

--- SICIER-SUR-Servidor/Almacenamiento/Acceso/Fachada.py
+++ SICIER-SUR-Servidor/Almacenamiento/Acceso/Fachada.py
@@ -1,4 +1,18 @@
 # Clase Persona
 # Daniel Henao
 
+import psycop2, psycop2.extras
+
+class FachadaBD:
+	
+	
+	def conectarBD(self):
+		cn = psycopg2.connect("host=localhost dbname=danielha user=danieha password=danielha")
+		cur = cn.cursor()  
+		cur.execute("SELECT campo1, campo2 FROM prueba")
+		for fila in cur:
+			campo1, campo2 = fila # extraer los campos
+		print campo1, campo2
+	
+
 
