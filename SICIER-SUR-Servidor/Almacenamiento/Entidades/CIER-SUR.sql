471a3fd3c478dca808fc197c05ae9376aa2a8f6b
crisefd <ophiuco93@hotmail.com>
2015-04-18 21:30:34 -0500

    Definicion inicial del modelo de BD

---
+++ SICIER-SUR-Servidor/Almacenamiento/Entidades/CIER-SUR.sql
@@ -0,0 +1,148 @@
+﻿
+DROP TABLE IF EXISTS Person CASCADE;
+CREATE TABLE Person(
+	id VARCHAR(50) PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	tel_num VARCHAR(50) NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL
+
+);
+
+DROP TABLE IF EXISTS Coordinator CASCADE;
+CREATE TABLE Coordinator(
+	id VARCHAR(50) PRIMARY KEY,
+	id_person_fk VARCHAR(50),
+	FOREIGN KEY(id_person_fk)
+	REFERENCES Person(id) 
+);
+
+DROP TABLE IF EXISTS Administrator CASCADE;
+CREATE TABLE Administrator(
+	id VARCHAR(50) PRIMARY KEY,
+	id_person_fk VARCHAR(50),
+	FOREIGN KEY(id_person_fk)
+	REFERENCES Person(id) 
+);
+
+DROP TABLE IF EXISTS Teacher CASCADE;
+CREATE TABLE Teacher(
+	id VARCHAR(50) PRIMARY KEY,
+	id_person_fk VARCHAR(50),
+	FOREIGN KEY(id_person_fk)
+	REFERENCES Person(id) 
+);
+
+DROP TABLE IF EXISTS MasterTeacher CASCADE;
+CREATE TABLE MasterTeacher(
+	id VARCHAR(50) PRIMARY KEY,
+	id_teacher_fk VARCHAR(50),
+	FOREIGN KEY(id_teacher_fk)
+	REFERENCES Teacher(id) 
+);
+
+DROP TABLE IF EXISTS LeaderTeacher CASCADE;
+CREATE TABLE LeaderTeacher(
+	id VARCHAR(50) PRIMARY KEY,
+	id_teacher_fk VARCHAR(50),
+	FOREIGN KEY(id_teacher_fk)
+	REFERENCES Teacher(id) 
+);
+
+DROP TABLE IF EXISTS MT_labor_exp CASCADE;
+CREATE TABLE MT_labor_exp(
+	id_MT_fk VARCHAR(50) NOT NULL,
+	item VARCHAR(100) NOT NULL,
+	FOREIGN KEY(id_MT_fk)
+	REFERENCES MasterTeacher(id),
+	PRIMARY KEY (id_MT_fk, item)
+);
+
+DROP TABLE IF EXISTS LT_labor_exp CASCADE;
+CREATE TABLE LT_labor_exp(
+	id_LT_fk VARCHAR(50) NOT NULL,
+	item VARCHAR(100) NOT NULL,
+	FOREIGN KEY(id_LT_fk)
+	REFERENCES LeaderTeacher(id),
+	PRIMARY KEY (id_LT_fk, item)
+);
+
+DROP TABLE IF EXISTS MT_academic_backg CASCADE;
+CREATE TABLE MT_academic_backg(
+	id_MT_fk VARCHAR(50) NOT NULL,
+	item VARCHAR(100) NOT NULL,
+	FOREIGN KEY(id_MT_fk)
+	REFERENCES MasterTeacher(id),
+	PRIMARY KEY(id_MT_fk, item)
+);
+
+DROP TABLE IF EXISTS LT_academic_backg CASCADE;
+CREATE TABLE LT_academic_backg(
+	id_LT_fk VARCHAR(50) NOT NULL,
+	item VARCHAR(100) NOT NULL,
+	FOREIGN KEY(id_LT_fk)
+	REFERENCES LeaderTeacher(id),
+	PRIMARY KEY(id_LT_fk, item)
+);
+
+DROP TABLE IF EXISTS Course CASCADE;
+CREATE TABLE Course(
+	id VARCHAR(50) NOT NULL,
+	description VARCHAR(100) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS Course_Activity CASCADE;
+CREATE TABLE Course_Activity(
+	id_course_fk VARCHAR(50) NOT NULL,
+	activity SERIAL,
+	FOREIGN KEY(id_course_fk)
+	REFERENCES Course(id),
+	PRIMARY KEY(id_course_fk, activity)
+);
+
+DROP TABLE IF EXISTS Activity_Grade CASCADE;
+CREATE TABLE Activity_Grade(
+	id_course_fk VARCHAR(50) NOT NULL,
+	activity_fk INTEGER NOT NULL,
+	FOREIGN KEY(id_course_fk, activity_fk)
+	REFERENCES Course_Activity(id_course_fk, activity),
+	PRIMARY KEY(id_course_fk, activity_fk)
+);
+
+DROP TABLE IF EXISTS Course_Cohort CASCADE;
+CREATE TABLE Course_Cohort(
+	id_course_fk VARCHAR(50) NOT NULL,
+	cohort SERIAL NOT NULL,
+	FOREIGN KEY(id_course_fk)
+	REFERENCES Course(id),
+	PRIMARY KEY(id_course_fk, cohort)
+	
+);
+
+DROP TABLE IF EXISTS Enrollment CASCADE;
+CREATE TABLE Enrollment(
+	id_MT_fk VARCHAR(50) NOT NULL,
+	id_course_fk VARCHAR(50) NOT NULL,
+	def_grade FLOAT,
+	FOREIGN KEY(id_MT_fk)
+	REFERENCES MasterTeacher(id),
+	FOREIGN KEY(id_course_fk)
+	REFERENCES Course(id),
+	PRIMARY KEY(id_MT_fk, id_course_fk)
+	
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SICIER-SUR-Servidor/Almacenamiento/Entidades/modelosBD.py
@@ -0,0 +1,128 @@
+from peewee import *
+
+database = PostgresqlDatabase('CIER-SUR', **{'user': 'crisefd'})
+
+class UnknownField(object):
+    pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Course(BaseModel):
+    description = CharField()
+    id = CharField(primary_key=True)
+
+    class Meta:
+        db_table = 'course'
+
+class CourseActivity(BaseModel):
+    activity = IntegerField()
+    id_course_fk = ForeignKeyField(db_column='id_course_fk', rel_model=Course, to_field='id')
+
+    class Meta:
+        db_table = 'course_activity'
+        primary_key = CompositeKey('activity', 'id_course_fk')
+
+class ActivityGrade(BaseModel):
+    activity_fk = ForeignKeyField(db_column='activity_fk', rel_model=CourseActivity, related_name='course_activity_activity_fk_set', to_field='activity')
+    id_course_fk = ForeignKeyField(db_column='id_course_fk', rel_model=CourseActivity, related_name='course_activity_id_course_fk_set', to_field='activity')
+
+    class Meta:
+        db_table = 'activity_grade'
+        primary_key = CompositeKey('activity_fk', 'id_course_fk')
+
+class Person(BaseModel):
+    city = CharField()
+    email = CharField()
+    first_name = CharField()
+    id = CharField(primary_key=True)
+    last_name = CharField()
+    tel_num = CharField()
+
+    class Meta:
+        db_table = 'person'
+
+class Administrator(BaseModel):
+    id = CharField(primary_key=True)
+    id_person_fk = ForeignKeyField(db_column='id_person_fk', null=True, rel_model=Person, to_field='id')
+
+    class Meta:
+        db_table = 'administrator'
+
+class Coordinator(BaseModel):
+    id = CharField(primary_key=True)
+    id_person_fk = ForeignKeyField(db_column='id_person_fk', null=True, rel_model=Person, to_field='id')
+
+    class Meta:
+        db_table = 'coordinator'
+
+class CourseCohort(BaseModel):
+    cohort = IntegerField()
+    id_course_fk = ForeignKeyField(db_column='id_course_fk', rel_model=Course, to_field='id')
+
+    class Meta:
+        db_table = 'course_cohort'
+        primary_key = CompositeKey('cohort', 'id_course_fk')
+
+class Teacher(BaseModel):
+    id = CharField(primary_key=True)
+    id_person_fk = ForeignKeyField(db_column='id_person_fk', null=True, rel_model=Person, to_field='id')
+
+    class Meta:
+        db_table = 'teacher'
+
+class Masterteacher(BaseModel):
+    id = CharField(primary_key=True)
+    id_teacher_fk = ForeignKeyField(db_column='id_teacher_fk', null=True, rel_model=Teacher, to_field='id')
+
+    class Meta:
+        db_table = 'masterteacher'
+
+class Enrollment(BaseModel):
+    def_grade = FloatField(null=True)
+    id_course_fk = ForeignKeyField(db_column='id_course_fk', rel_model=Course, to_field='id')
+    id_mt_fk = ForeignKeyField(db_column='id_mt_fk', rel_model=Masterteacher, to_field='id')
+
+    class Meta:
+        db_table = 'enrollment'
+        primary_key = CompositeKey('id_course_fk', 'id_mt_fk')
+
+class Leaderteacher(BaseModel):
+    id = CharField(primary_key=True)
+    id_teacher_fk = ForeignKeyField(db_column='id_teacher_fk', null=True, rel_model=Teacher, to_field='id')
+
+    class Meta:
+        db_table = 'leaderteacher'
+
+class LtAcademicBackg(BaseModel):
+    id_lt_fk = ForeignKeyField(db_column='id_lt_fk', rel_model=Leaderteacher, to_field='id')
+    item = CharField()
+
+    class Meta:
+        db_table = 'lt_academic_backg'
+        primary_key = CompositeKey('id_lt_fk', 'item')
+
+class LtLaborExp(BaseModel):
+    id_lt_fk = ForeignKeyField(db_column='id_lt_fk', rel_model=Leaderteacher, to_field='id')
+    item = CharField()
+
+    class Meta:
+        db_table = 'lt_labor_exp'
+        primary_key = CompositeKey('id_lt_fk', 'item')
+
+class MtAcademicBackg(BaseModel):
+    id_mt_fk = ForeignKeyField(db_column='id_mt_fk', rel_model=Masterteacher, to_field='id')
+    item = CharField()
+
+    class Meta:
+        db_table = 'mt_academic_backg'
+        primary_key = CompositeKey('id_mt_fk', 'item')
+
+class MtLaborExp(BaseModel):
+    id_mt_fk = ForeignKeyField(db_column='id_mt_fk', rel_model=Masterteacher, to_field='id')
+    item = CharField()
+
+    class Meta:
+        db_table = 'mt_labor_exp'
+        primary_key = CompositeKey('id_mt_fk', 'item')
